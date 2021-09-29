9eba19c9616a3aa344cef556dc2e3c81c9f6cf36
Veljko <veljomiha@gmail.com>
2021-09-29 10:43:46 +0200

    mobile

--- main.js
+++ main.js
@@ -1,39 +1,27 @@
-function validateemail()  
-{  
-var x=document.myform.email.value;  
-var atposition=x.indexOf("@");  
-var dotposition=x.lastIndexOf(".");
-// var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/
-var message = document.getElementById("message");
-if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length)
-// if (filter.test(x.value))
-{  
-    message.innerHTML ="<span style='color:red'>Oops! Please check your email</span>";
-  return false;
-  }
-  else{
-    message.innerHTML = "<span style='color:green'>Your email is correct</span>";
-  return false;  
-  }
-}  
-
-// var testresults
-
-// function checkemail() {
-//   var str = document.myform.email.value
-//   var filter = /^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/;
-//   if (filter.test(str))
-//     testresults = true
-//   else {
-//     alert("Please input a valid email address!")
-//     testresults = false
+// function validateemail()  
+// {  
+// var x=document.myform.email.value;  
+// var atposition=x.indexOf("@");  
+// var dotposition=x.lastIndexOf(".");
+// var message = document.getElementById("message");
+// if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length)
+// {  
+//     message.innerHTML ="<span style='color:red'>Oops! Please check your email</span>";
+//   return false;
 //   }
-//   return (testresults)
-// }
+//   else{
+//     message.innerHTML = "<span style='color:green'>Your email is correct</span>";
+//   return false;  
+//   }
+// } 
 
-// function checkbae() {
-//   if (document.layers || document.getElementById)
-//     return checkemail()
-//   else
-//     return true
-// }
+function validateemail() {
+
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myform.email.value))
+  {
+    message.innerHTML = "<span style='color:green'>Your email is correct</span>";
+    return (false);
+  }
+  message.innerHTML ="<span style='color:red'>Oops! Please check your email</span>";
+    return (false);
+}
